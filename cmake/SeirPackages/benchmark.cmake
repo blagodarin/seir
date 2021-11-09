2aca02c6f9ad6a538e46a881337fe30245a41c4e
Sergei Blagodarin <s.blagodarin@gmail.com>
2021-11-09 22:42:31 +0300

    Add audio library benchmarks

--- cmake/SeirPackages/benchmark.cmake
+++ cmake/SeirPackages/benchmark.cmake
@@ -3,15 +3,21 @@
 # SPDX-License-Identifier: Apache-2.0
 
 function(seir_provide_benchmark _output)
-	cmake_parse_arguments(_arg "" "SET_UPDATED" "" ${ARGN})
+	cmake_parse_arguments(_arg "" "SET_UPDATED;STATIC_RUNTIME" "" ${ARGN})
 	if(_arg_SET_UPDATED)
 		set(${_arg_SET_UPDATED} OFF PARENT_SCOPE)
+	endif()
+	if(_arg_STATIC_RUNTIME)
+		set(_patch ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/benchmark.patch)
+	else()
+		set(_patch "")
 	endif()
 	set(_version "1.6.0")
 	set(_package "benchmark-${_version}")
 	seir_download("https://github.com/google/benchmark/archive/refs/tags/v${_version}.zip"
 		SHA1 "26de5f5c784a4cdfaff7899fa2ba2a3d1b02fdf7"
 		EXTRACT_DIR "${_package}"
+		PATCH ${_patch}
 		RESULT _downloaded)
 	set(_install_dir ${SEIR_PACKAGE_DIR}/benchmark)
 	if(_downloaded OR NOT EXISTS ${_install_dir})
@@ -20,6 +26,7 @@ function(seir_provide_benchmark _output)
 		message(STATUS "[SEIR] Building benchmark from ${_source_dir}")
 		_seir_cmake(${_source_dir} ${_build_dir} ${_install_dir} OPTIONS
 			-DCMAKE_CXX_STANDARD=${CMAKE_CXX_STANDARD}
+			-DCMAKE_POLICY_DEFAULT_CMP0091=NEW # MSVC runtime library flags are selected by an abstraction.
 			-DBENCHMARK_ENABLE_GTEST_TESTS=OFF
 			-DBENCHMARK_ENABLE_TESTING=OFF
 			)
---
+++ libs/audio/benchmarks/src/processing.cpp
@@ -0,0 +1,126 @@
+// This file is part of Seir.
+// Copyright (C) Sergei Blagodarin.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <seir_audio/processing.hpp>
+#include <seir_base/buffer.hpp>
+
+#include <algorithm>
+#include <numeric>
+
+#include <benchmark/benchmark.h>
+
+namespace
+{
+	void baseline_addSamples1D_i16(float* dst, const int16_t* src, size_t length) noexcept
+	{
+		constexpr auto unit = 1.f / 32768.f;
+		for (size_t i = 0; i < length; ++i)
+			dst[i] += static_cast<float>(src[i]) * unit;
+	}
+
+	template <typename T, auto function>
+	void benchmark_addSamples1D(benchmark::State& state)
+	{
+		seir::Buffer<T, seir::AlignedAllocator<seir::kAudioAlignment>> src{ static_cast<size_t>(state.range(0) - 1) / sizeof(T) };
+		std::iota(src.data(), src.data() + src.capacity(), T{});
+		seir::Buffer<float, seir::AlignedAllocator<seir::kAudioAlignment>> dst{ src.capacity() };
+		std::iota(dst.data(), dst.data() + dst.capacity(), 0.f);
+		for (auto _ : state)
+			function(dst.data(), src.data(), src.capacity());
+	}
+
+	void addSamples1D_i16_Opt(benchmark::State& state) { benchmark_addSamples1D<int16_t, static_cast<void (*)(float*, const int16_t*, size_t)>(seir::addSamples1D)>(state); }
+	void addSamples1D_i16_Ref(benchmark::State& state) { benchmark_addSamples1D<int16_t, baseline_addSamples1D_i16>(state); }
+}
+
+BENCHMARK(addSamples1D_i16_Opt)->Arg(seir::kAudioAlignment)->Arg(2 * seir::kAudioAlignment)->RangeMultiplier(4)->Range(1 << 10, 1 << 20);
+BENCHMARK(addSamples1D_i16_Ref)->Arg(seir::kAudioAlignment)->Arg(2 * seir::kAudioAlignment)->RangeMultiplier(4)->Range(1 << 10, 1 << 20);
+
+namespace
+{
+	void baseline_addSamples2x1D_i16(float* dst, const int16_t* src, size_t length) noexcept
+	{
+		constexpr auto unit = 1.f / 32768.f;
+		for (; length > 0; --length)
+		{
+			const auto value = static_cast<float>(*src++) * unit;
+			*dst++ += value;
+			*dst++ += value;
+		}
+	}
+
+	void baseline_addSamples2x1D_f32(float* dst, const float* src, size_t length) noexcept
+	{
+		for (; length > 0; --length)
+		{
+			const auto value = *src++;
+			*dst++ += value;
+			*dst++ += value;
+		}
+	}
+
+	template <typename T, auto function>
+	void benchmark_addSamples2x1D(benchmark::State& state)
+	{
+		seir::Buffer<T, seir::AlignedAllocator<seir::kAudioAlignment>> src{ static_cast<size_t>(state.range(0) - 1) / sizeof(T) };
+		std::iota(src.data(), src.data() + src.capacity(), T{});
+		seir::Buffer<float, seir::AlignedAllocator<seir::kAudioAlignment>> dst{ src.capacity() * 2 };
+		std::iota(dst.data(), dst.data() + dst.capacity(), 0.f);
+		for (auto _ : state)
+			function(dst.data(), src.data(), src.capacity());
+	}
+
+	void addSamples2x1D_i16_Opt(benchmark::State& state) { benchmark_addSamples2x1D<int16_t, static_cast<void (*)(float*, const int16_t*, size_t)>(seir::addSamples2x1D)>(state); }
+	void addSamples2x1D_i16_Ref(benchmark::State& state) { benchmark_addSamples2x1D<int16_t, baseline_addSamples2x1D_i16>(state); }
+	void addSamples2x1D_f32_Opt(benchmark::State& state) { benchmark_addSamples2x1D<float, static_cast<void (*)(float*, const float*, size_t)>(seir::addSamples2x1D)>(state); }
+	void addSamples2x1D_f32_Ref(benchmark::State& state) { benchmark_addSamples2x1D<float, baseline_addSamples2x1D_f32>(state); }
+}
+
+BENCHMARK(addSamples2x1D_i16_Opt)->Arg(seir::kAudioAlignment)->Arg(2 * seir::kAudioAlignment)->RangeMultiplier(4)->Range(1 << 10, 1 << 20);
+BENCHMARK(addSamples2x1D_i16_Ref)->Arg(seir::kAudioAlignment)->Arg(2 * seir::kAudioAlignment)->RangeMultiplier(4)->Range(1 << 10, 1 << 20);
+BENCHMARK(addSamples2x1D_f32_Opt)->Arg(seir::kAudioAlignment)->Arg(2 * seir::kAudioAlignment)->RangeMultiplier(4)->Range(1 << 10, 1 << 20);
+BENCHMARK(addSamples2x1D_f32_Ref)->Arg(seir::kAudioAlignment)->Arg(2 * seir::kAudioAlignment)->RangeMultiplier(4)->Range(1 << 10, 1 << 20);
+
+namespace
+{
+	void baseline_duplicate1D_i16(int16_t* dst, const int16_t* src, size_t length) noexcept
+	{
+		for (; length > 0; --length)
+		{
+			const auto value = *src++;
+			*dst++ = value;
+			*dst++ = value;
+		}
+	}
+
+	void baseline_duplicate1D_i32(int32_t* dst, const int32_t* src, size_t srcLength) noexcept
+	{
+		for (; srcLength > 0; --srcLength)
+		{
+			const auto value = *src++;
+			*dst++ = value;
+			*dst++ = value;
+		}
+	}
+
+	template <typename T, auto function>
+	void benchmark_duplicate1D(benchmark::State& state)
+	{
+		seir::Buffer<T, seir::AlignedAllocator<seir::kAudioAlignment>> src{ static_cast<size_t>(state.range(0) - 1) / sizeof(T) };
+		std::iota(src.data(), src.data() + src.capacity(), T{});
+		seir::Buffer<T, seir::AlignedAllocator<seir::kAudioAlignment>> dst{ src.capacity() * 2 };
+		for (auto _ : state)
+			function(dst.data(), src.data(), src.capacity());
+	}
+
+	void duplicate1D_i16_Opt(benchmark::State& state) { benchmark_duplicate1D<int16_t, seir::duplicate1D_16>(state); }
+	void duplicate1D_i16_Ref(benchmark::State& state) { benchmark_duplicate1D<int16_t, baseline_duplicate1D_i16>(state); }
+	void duplicate1D_i32_Opt(benchmark::State& state) { benchmark_duplicate1D<int32_t, seir::duplicate1D_32>(state); }
+	void duplicate1D_i32_Ref(benchmark::State& state) { benchmark_duplicate1D<int32_t, baseline_duplicate1D_i32>(state); }
+}
+
+BENCHMARK(duplicate1D_i16_Opt)->Arg(seir::kAudioAlignment)->Arg(2 * seir::kAudioAlignment)->RangeMultiplier(4)->Range(1 << 10, 1 << 20);
+BENCHMARK(duplicate1D_i16_Ref)->Arg(seir::kAudioAlignment)->Arg(2 * seir::kAudioAlignment)->RangeMultiplier(4)->Range(1 << 10, 1 << 20);
+BENCHMARK(duplicate1D_i32_Opt)->Arg(seir::kAudioAlignment)->Arg(2 * seir::kAudioAlignment)->RangeMultiplier(4)->Range(1 << 10, 1 << 20);
+BENCHMARK(duplicate1D_i32_Ref)->Arg(seir::kAudioAlignment)->Arg(2 * seir::kAudioAlignment)->RangeMultiplier(4)->Range(1 << 10, 1 << 20);
