4bb3cca0f50a6db51e975735dd6b16e1c4e9ac18
Sergei Blagodarin <blagodarin@yandex-team.ru>
2025-09-21 19:09:04 +0300

    Update Core Audio stub

--- libs/audio/src/backend_coreaudio.mm
+++ libs/audio/src/backend_coreaudio.mm
@@ -6,12 +6,21 @@
 
 #include <seir_audio/player.hpp>
 #include <seir_base/scope.hpp>
+#include "common.hpp"
+
+#include <array>
 
 #include <AudioToolbox/AudioToolbox.h>
 
 namespace
 {
-	void outputCallback(void*, OpaqueAudioQueue*, AudioQueueBuffer*)
+	constexpr uint32_t makeBufferBytes(uint32_t minFrames) noexcept
+	{
+		const auto minBytes = minFrames * seir::kAudioFrameSize;
+		return (minBytes + seir::kAudioBlockSize - 1) / seir::kAudioBlockSize * seir::kAudioBlockSize;
+	}
+
+	void outputCallback(void*, AudioQueueRef, AudioQueueBufferRef)
 	{
 	}
 }
@@ -25,6 +34,7 @@ namespace seir
 				switch (status)
 				{
 				case kAudioFileUnsupportedDataFormatError: return "kAudioFileUnsupportedDataFormatError";
+				case kAudioQueueErr_BufferEmpty: return "kAudioQueueErr_BufferEmpty";
 				}
 				return nullptr;
 			}();
@@ -36,10 +46,10 @@ namespace seir
 			.mSampleRate = static_cast<Float64>(preferredSamplingRate),
 			.mFormatID = kAudioFormatLinearPCM,
 			.mFormatFlags = kAudioFormatFlagIsFloat,
-			.mBytesPerPacket = 8,
+			.mBytesPerPacket = kAudioFrameSize,
 			.mFramesPerPacket = 1,
-			.mBytesPerFrame = 8,
-			.mChannelsPerFrame = 2,
+			.mBytesPerFrame = kAudioFrameSize,
+			.mChannelsPerFrame = kAudioChannels,
 			.mBitsPerChannel = 32,
 			.mReserved = 0,
 		};
@@ -50,6 +60,29 @@ namespace seir
 			::AudioQueueStop(queue, true);
 			::AudioQueueDispose(queue, false);
 		} };
-		// TODO: Implement.
+		std::array<AudioQueueBufferRef, 3> buffers{};
+		constexpr auto bufferBytes = ::makeBufferBytes(0x1000);
+		if (const auto status = ::AudioQueueAllocateBuffer(queue, bufferBytes, &buffers[0]))
+			return error("AudioQueueAllocateBuffer", status);
+		SEIR_FINALLY{ [&]() noexcept { ::AudioQueueFreeBuffer(queue, buffers[0]); } };
+		buffers[0]->mAudioDataByteSize = bufferBytes;
+		if (const auto status = ::AudioQueueAllocateBuffer(queue, bufferBytes, &buffers[1]))
+			return error("AudioQueueAllocateBuffer", status);
+		SEIR_FINALLY{ [&]() noexcept { ::AudioQueueFreeBuffer(queue, buffers[1]); } };
+		buffers[1]->mAudioDataByteSize = bufferBytes;
+		if (const auto status = ::AudioQueueAllocateBuffer(queue, bufferBytes, &buffers[2]))
+			return error("AudioQueueAllocateBuffer", status);
+		SEIR_FINALLY{ [&]() noexcept { ::AudioQueueFreeBuffer(queue, buffers[2]); } };
+		buffers[2]->mAudioDataByteSize = bufferBytes;
+		constexpr auto bufferFrames = bufferBytes / kAudioFrameSize;
+		callbacks.onBackendAvailable(preferredSamplingRate, bufferFrames);
+		for (size_t i = 0; callbacks.onBackendIdle(); i = (i + 1) % buffers.size())
+		{
+			const auto writtenFrames = callbacks.onBackendRead(static_cast<float*>(buffers[i]->mAudioData), bufferFrames);
+			std::memset(static_cast<std::byte*>(buffers[i]->mAudioData) + writtenFrames * kAudioFrameSize, 0, (bufferFrames - writtenFrames) * kAudioFrameSize);
+			if (const auto status = ::AudioQueueEnqueueBuffer(queue, buffers[i], 0, nullptr))
+				return error("AudioQueueEnqueueBuffer", status);
+			break; // TODO: Implement.
+		}
 	}
 }
