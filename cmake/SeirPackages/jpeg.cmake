da6594463395006faff341efe37e56b5ce8a0cd8
Sergei Blagodarin <blagodarin@yandex-team.ru>
2026-04-27 00:02:04 +0300

    Update libjpeg-turbo to 3.1.4.1

--- cmake/SeirPackages/jpeg.cmake
+++ cmake/SeirPackages/jpeg.cmake
@@ -8,11 +8,20 @@ include(${CMAKE_CURRENT_LIST_DIR}/nasm.cmake)
 function(seir_provide_jpeg result)
 	cmake_parse_arguments(arg "FLAG" "SET_UPDATED;STATIC_RUNTIME" "" ${ARGN})
 	_seir_provide_begin("jpeg")
-	seir_provide_nasm(nasm_flag FLAG SET_UPDATED nasm_updated)
-	set(version "3.0.3")
+	if(WIN32)
+		seir_provide_nasm(nasm_flag FLAG SET_UPDATED nasm_updated)
+		seir_select(with_crt_dll ${arg_STATIC_RUNTIME} OFF ON)
+		set(win32_options
+			-DWITH_CRT_DLL=${with_crt_dll}
+			${nasm_flag}
+			)
+	else()
+		set(win32_options "")
+	endif()
+	set(version "3.1.4.1")
 	set(package "libjpeg-turbo-${version}")
 	seir_download("https://github.com/libjpeg-turbo/libjpeg-turbo/releases/download/${version}/${package}.tar.gz"
-		SHA256 "343e789069fc7afbcdfe44dbba7dbbf45afa98a15150e079a38e60e44578865d"
+		SHA256 "ecae8008e2cc9ade2f2c1bb9d5e6d4fb73e7c433866a056bd82980741571a022"
 		EXTRACT_DIR "${package}"
 		RESULT downloaded
 		)
@@ -21,22 +30,15 @@ function(seir_provide_jpeg result)
 		set(source_dir ${CMAKE_BINARY_DIR}/${package})
 		set(build_dir ${source_dir}-build)
 		message(STATUS "[SEIR] Building JPEG from ${source_dir}")
-		seir_select(with_crt_dll ${arg_STATIC_RUNTIME} OFF ON)
-		_seir_cmake(${source_dir} ${build_dir} ${install_dir} TARGET jpeg-static OPTIONS
+		_seir_cmake(${source_dir} ${build_dir} ${install_dir} OPTIONS
 			-DENABLE_SHARED=OFF
 			-DREQUIRE_SIMD=ON
 			-DWITH_ARITH_DEC=OFF
 			-DWITH_ARITH_ENC=OFF
-			-DWITH_CRT_DLL=${with_crt_dll}
+			-DWITH_TESTS=OFF
+			-DWITH_TOOLS=OFF
 			-DWITH_TURBOJPEG=OFF
-			${nasm_flag}
-			)
-		file(INSTALL
-			${build_dir}/jconfig.h
-			${source_dir}/jerror.h
-			${source_dir}/jmorecfg.h
-			${source_dir}/jpeglib.h
-			DESTINATION ${install_dir}/include
+			${win32_options}
 			)
 		message(STATUS "[SEIR] Provided JPEG at ${install_dir}")
 		if(arg_SET_UPDATED)
