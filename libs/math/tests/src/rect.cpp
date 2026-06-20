aec1027d8a5b3992ade50ed59adb1a70ed00427c
Sergei Blagodarin <blagodarin@yandex-team.ru>
2026-06-21 02:16:40 +0300

    add more Rect tests

--- libs/math/tests/src/rect.cpp
+++ libs/math/tests/src/rect.cpp
@@ -43,7 +43,7 @@ TEST_CASE("Rect::Rect(Vec2, Size2D)")
 	CHECK(r.height() == 5.f);
 }
 
-TEST_CASE("Rect::Rect(SizeD)")
+TEST_CASE("Rect::Rect(Size2D)")
 {
 	const Rect r{ Size2D{ 1, 2 } };
 	CHECK(r.left() == 0.f);
@@ -77,4 +77,147 @@ TEST_CASE("Rect::bound(Vec2)")
 	CHECK(r.bound({ 1, 3 }) == Vec2(1, 2));
 	CHECK(r.bound({ 2, 3 }) == Vec2(2, 2));
 	CHECK(r.bound({ 4, 3 }) == Vec2(2, 2));
+}
+
+TEST_CASE("Rect::contains(Vec2)")
+{
+	const Rect r{ { 1, 2 }, Vec2{ 3, 4 } };
+	SUBCASE("top left")
+	{
+		CHECK_FALSE(r.contains(Vec2{ 0.9, 1.9 }));
+		CHECK_FALSE(r.contains(Vec2{ 0.9, 2.0 }));
+		CHECK_FALSE(r.contains(Vec2{ 1.0, 1.9 }));
+		CHECK(r.contains(Vec2{ 1.0, 2.0 }));
+	}
+	SUBCASE("top right")
+	{
+		CHECK_FALSE(r.contains(Vec2{ 2.9, 1.9 }));
+		CHECK(r.contains(Vec2{ 2.9, 2.0 }));
+		CHECK_FALSE(r.contains(Vec2{ 3.0, 1.9 }));
+		CHECK_FALSE(r.contains(Vec2{ 3.0, 2.0 }));
+	}
+	SUBCASE("bottom left")
+	{
+		CHECK_FALSE(r.contains(Vec2{ 0.9, 3.9 }));
+		CHECK_FALSE(r.contains(Vec2{ 0.9, 4.0 }));
+		CHECK(r.contains(Vec2{ 1.0, 3.9 }));
+		CHECK_FALSE(r.contains(Vec2{ 1.0, 4.0 }));
+	}
+	SUBCASE("bottom right")
+	{
+		CHECK(r.contains(Vec2{ 2.9, 3.9 }));
+		CHECK_FALSE(r.contains(Vec2{ 2.9, 4.0 }));
+		CHECK_FALSE(r.contains(Vec2{ 3.0, 3.9 }));
+		CHECK_FALSE(r.contains(Vec2{ 3.0, 4.0 }));
+	}
+}
+
+TEST_CASE("Rect::contains(Rect::bound(Vec2))")
+{
+	const Rect r{ { 1, 2 }, Vec2{ 3, 4 } };
+	SUBCASE("top left")
+	{
+		CHECK(r.contains(r.bound({ 0, 1 })));
+	}
+	SUBCASE("top center")
+	{
+		CHECK(r.contains(r.bound({ 2, 1 })));
+	}
+	SUBCASE("top right")
+	{
+		CHECK_FALSE(r.contains(r.bound({ 4, 1 }))); // Corner case!
+	}
+	SUBCASE("middle left")
+	{
+		CHECK(r.contains(r.bound({ 0, 3 })));
+	}
+	SUBCASE("center")
+	{
+		CHECK(r.contains(r.bound({ 2, 3 })));
+	}
+	SUBCASE("middle right")
+	{
+		CHECK_FALSE(r.contains(r.bound({ 4, 3 })));
+	}
+	SUBCASE("bottom left")
+	{
+		CHECK_FALSE(r.contains(r.bound({ 0, 5 }))); // Corner case!
+	}
+	SUBCASE("bottom center")
+	{
+		CHECK_FALSE(r.contains(r.bound({ 2, 5 }))); // Corner case!
+	}
+	SUBCASE("bottom right")
+	{
+		CHECK_FALSE(r.contains(r.bound({ 4, 5 }))); // Corner case!
+	}
+}
+
+TEST_CASE("Rect::intersects(Rect)")
+{
+	const Rect r{ { 1, 2 }, Vec2{ 4, 5 } };
+	SUBCASE("exact match")
+	{
+		CHECK(r.intersects(r));
+	}
+	SUBCASE("top left / bottom right")
+	{
+		SUBCASE("touch corner")
+		{
+			const Rect other{ { 0, 0 }, Vec2{ 1, 2 } };
+			CHECK_FALSE(r.intersects(other));
+			CHECK_FALSE(other.intersects(r));
+		}
+		SUBCASE("touch vertical side")
+		{
+			const Rect other{ { 0, 0 }, Vec2{ 1, 3 } };
+			CHECK_FALSE(r.intersects(other));
+			CHECK_FALSE(other.intersects(r));
+		}
+		SUBCASE("touch horizontal side")
+		{
+			const Rect other{ { 0, 0 }, Vec2{ 2, 2 } };
+			CHECK_FALSE(r.intersects(other));
+			CHECK_FALSE(other.intersects(r));
+		}
+		SUBCASE("intersect corner")
+		{
+			const Rect other{ { 0, 0 }, Vec2{ 2, 3 } };
+			CHECK(r.intersects(other));
+			CHECK(other.intersects(r));
+		}
+	}
+	SUBCASE("top right / bottom left")
+	{
+		SUBCASE("touch corner")
+		{
+			const Rect other{ { 4, 0 }, Vec2{ 5, 2 } };
+			CHECK_FALSE(r.intersects(other));
+			CHECK_FALSE(other.intersects(r));
+		}
+		SUBCASE("touch vertical side")
+		{
+			const Rect other{ { 4, 0 }, Vec2{ 5, 3 } };
+			CHECK_FALSE(r.intersects(other));
+			CHECK_FALSE(other.intersects(r));
+		}
+		SUBCASE("touch horizontal side")
+		{
+			const Rect other{ { 3, 0 }, Vec2{ 5, 2 } };
+			CHECK_FALSE(r.intersects(other));
+			CHECK_FALSE(other.intersects(r));
+		}
+		SUBCASE("intersect corner")
+		{
+			const Rect other{ { 3, 0 }, Vec2{ 5, 3 } };
+			CHECK(r.intersects(other));
+			CHECK(other.intersects(r));
+		}
+	}
+	SUBCASE("inside / outside")
+	{
+		const Rect other{ { 0, 1 }, Vec2{ 5, 6 } };
+		CHECK(r.intersects(other));
+		CHECK(other.intersects(r));
+	}
 }
